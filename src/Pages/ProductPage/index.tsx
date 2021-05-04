3a5993df3db1fab42d6465c179c78b77b56ae88c
VoXzE <sinncere520@gmail.com>
2021-05-04 13:20:27 -0400

    feat: created QuantityInput component

--- src/Pages/ProductPage/index.tsx
+++ src/Pages/ProductPage/index.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import QuantityInput from "../../components/QuantityInput";
+import "./index.scss";
 
 const ProductPage = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const incrementQty = () => setQuantity(quantity + 1);
+  const decrementQty = () => setQuantity(quantity - 1);
+
   return (
     <div className="product-page">
-      Product Page
+      <h1 className="product-name">Product A</h1>
+
+      <div className="main-info">
+        <img src="https://i.imgur.com/r5Y0xpy.jpg" alt="product-img" className="thumbnail" />
+        <p className="description">1233123123123123123123131313</p>
+      </div>
+
+      <h1 className="price">$12.99</h1>
+
+      <div className="qty-info">
+        <p>Quantity</p>
+        <QuantityInput value={quantity} increment={incrementQty} decrement={decrementQty} onChange={setQuantity} />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/QuantityInput/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.scss";
+
+interface QuantityInputProps {
+  increment: () => void;
+  decrement: () => void;
+  onChange: (val: number) => void;
+  value: number;
+}
+
+const QuantityInput = ({ increment, decrement, onChange, value }: QuantityInputProps) => {
+  return (
+    <div className="qty-selector">
+      <button className="qty-btn" onClick={decrement}>-</button>
+      <input value={value} className="input" type="number" onChange={(e => onChange(parseInt(e.target.value)))} />
+      <button className="qty-btn"  onClick={increment}>+</button>
+    </div>
+  );
+}
+
+export default QuantityInput;
