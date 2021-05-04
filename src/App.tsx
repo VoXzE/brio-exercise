3691b6ad2b52030be098613c55570b102fde65c9
VoXzE <sinncere520@gmail.com>
2021-05-04 12:47:41 -0400

    feat: created pages boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// Components
+import CheckoutPage from "./Pages/CheckoutPage";
+import HomePage from "./Pages/HomePage";
+import ProductPage from "./Pages/ProductPage";
+
+// Styles
 import "./assets/css/main.scss";
 
 const App = () => {
   return (
     <div className="App">
-      132133
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/product/:id" component={ProductPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/ProductPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductPage = () => {
+  return (
+    <div className="product-page">
+      Product Page
+    </div>
+  );
+}
+
+export default ProductPage;
